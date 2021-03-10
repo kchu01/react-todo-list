21d4abf41ea379c4f6fe9e45eefa59ccba226be0
kchu01 <chukathy01@gmail.com>
2021-03-09 17:06:32 -0800

    MVP

--- src/components/todo-list/TodoList.jsx
+++ src/components/todo-list/TodoList.jsx
@@ -3,20 +3,62 @@ import ListItem from '../list-item/ListItem.jsx'
 import placeHolderTasks from './placeHolderTasks'
 
 export default class TodoList extends Component {
+    state = {
+        taskArray: placeHolderTasks,
+        newItemInput: ''
+    }
 
+    // event handlers
+    handleClearList = () => {
+        this.setState({
+            taskArray: []
+        }, () => console.log('cleared the list', this.state))
+    }
+
+    handleNewItemChange = (e) => {
+        this.setState({
+            newItemInput: e.target.value
+        }, () => console.log(this.state))
+    }
+
+    handleNewItemSubmit = (e) => {
+        e.preventDefault()
+        this.setState((prevState, props) => {
+            return {
+                taskArray: [...prevState.taskArray, this.state.newItemInput]
+            }
+        }, () => this.setState({ newItemInput: '' }))
+    }
 
     render() {
-        const listItems = placeHolderTasks.map((placeHolderTask, index) => {
+        const listItems = this.state.taskArray.map((placeHolderTask, index) => {
             return (
-                <ListItem task={placeHolderTask} key={`ListItem ${index}`} />
+                <ListItem
+                    task={placeHolderTask}
+                    key={`ListItem ${index}`} />
             )
         })
         return (
             <div>
                 <h1> Things I should stop procrastinating:</h1>
+
+                <form onSubmit={this.handleNewItemSubmit}>
+                    <input type="text"
+                        placeholder="Type an item here"
+                        onChange={this.handleNewItemChange}
+                        value={this.state.newItemInput}
+                    />
+
+                    <input
+                        type='submit'
+                        value='Add item'
+                    />
+                </form>
+
                 <ul>
                     {listItems}
                 </ul>
+                <button onClick={this.handleClearList} >Finished the list!</button>
             </div>
         )
     }
